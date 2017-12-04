df195fb40ba35ae092da55f292f358e992a59435
Freddy Persson <FreddyPersson85@gmail.com>
2017-12-04 13:45:02 +0100

    New start, working version

--- app.js
+++ app.js
@@ -1,9 +1,53 @@
-const nodegit = require('simple-git/promise');
+const simpleGit = require("simple-git/promise");
+const path = require('path');
+const basePath = '../';
+const targets = require('./targets.json');
+const pm = require('promisemaker');
+const exec = pm(require('child_process')).exec;
 
-async function pull(repoPath, branch) {
-	let repo = simpleGit(repoPath);
-	let status = await repo.pull('origin', 'master');
-	console.log(status)
+// Checkout a branch and make a pull for that branch
+async function pull(repoPath, branch){
+  	let repo, status, err;
+  	try {
+		repo = simpleGit(repoPath);
+		await repo.checkout(branch);
+		status = await repo.pull('origin', branch);
+  	} catch(e) {
+		err = e;
+  	}
+
+  	let changed = status.files && status.files.length != 0;
+
+  	if(changed) {
+		console.log('pull-success', repoPath, branch, status);
+  	}
+  	if(err) {
+		console.log('pull-error', repoPath, branch, err);
+  	}
+	if(changed && run) {
+		for(let cmd of run) {
+			let err, result = await exec(
+				cmd,
+				{cwd: repoPath}
+			).catch((e) => err = e);
+			if(result) {
+				console.log('Running ', cmd, '\n', result);
+			}
+			if(err) {
+				console.log('Error running ', cmd, '\n', err);
+			}
+		}
+	}
 }
 
-pull('C:/GitRepo/scraper');
+// Every 10 seconds loop through our repos
+// and call the pull function
+setInterval(()=>{
+	for(let target of targets) {
+		pull(
+			path.join(basePath, target.path),
+			target.branch,
+			target.run
+		);
+	}
+}, 1000);
